a83ff2c861913e2757850a4150073f67411d6420
Clary <claryphp@metropolia.fi>
2026-09-01 13:26:15 +0300

    feat(api): create tours

--- tour-api/tourHandlers.js
+++ tour-api/tourHandlers.js
@@ -6,7 +6,15 @@ const getAllTours = (req, res) => {
 };
 
 const createTour = (req, res) => {
-    res.json({ message: "Hello from create tour!" });
+    const { name, info, image, price, location } = req.body;
+    const tour = Tour.addOne(
+        name,
+        info,
+        image,
+        price,
+        location
+    );
+    res.status(201).json(tour);
 };
 
 const getTourById = (req, res) => {
